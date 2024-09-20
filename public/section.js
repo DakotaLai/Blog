76f8d1225c91effa88609f3e523396ca82ad5657
DakotaLai <lai.hon@northeastern.edu>
2024-09-20 21:57:32 +1000

    updated to increase code efficiency

--- public/section.js
+++ public/section.js
@@ -1,13 +1,31 @@
+const fullUrl = window.location.href;
+const lastSeg = fullUrl.split("/").pop();
+console.log(lastSeg);
+
+
 document.addEventListener("DOMContentLoaded", function () {
     // console.log("ABC");
-
+    const articles = {
+        "codeIpsum": ['/database/codeIpsum/article1',
+            '/database/codeIpsum/article2',
+            '/database/codeIpsum/article3',
+            '/database/codeIpsum/article4'],
+        "catIpsum": [
+            '/database/catIpsum/article1',
+            '/database/catIpsum/article3',
+            '/database/catIpsum/article4',
+            '/database/catIpsum/article5',
+        ],
+        "cakeIpsum": [
+            '/database/cakeIpsum/article1',
+            '/database/cakeIpsum/article2',
+            '/database/cakeIpsum/article3',
+        ]
+    }
     var date = new Date().toJSON().slice(0, 10);
-    const files = [
-        '/database/codeIpsum/article1',
-        '/database/codeIpsum/article2',
-        '/database/codeIpsum/article3',
-        '/database/codeIpsum/article4',
-    ];
+
+    const files = articles[lastSeg];
+    console.log(files)
     Promise.all(files.map(file => fetch(file).then(response => response.json())))
         .then(data => {
             const dataDisplay = document.getElementById("data-display");
@@ -22,12 +40,17 @@ document.addEventListener("DOMContentLoaded", function () {
                 <div class="d-flex justify-content-between" id="data-display">
                     <div class="card-body">
                         <h3 class="card-title">${element.title}</h3>
+                        <hr>
+                        <div class="mt-3">
                         <form action="/${element.id}" method="POST" class="card-link" data-id=${element.id} >
                         <input type="hidden" name="articleId" value="${element.id}">
-                        <input type="submit" class="view-article btn btn-dark" value="View Article">
+                       
+                            <input type="submit" class="view-article btn btn-dark" value="View Article">
+                       
                     </form>
                     </div>
-                    <div class="card-footer"> 
+                    </div>
+                    <div class="card-footer d-none d-sm-block d-md-block d-xl-block d-lg-block"> 
                         <p class="card-author" id="card-author">By: ${element.author}</p> 
                     <p class="card-date"> Date: ${date}</p> 
                     </div>
